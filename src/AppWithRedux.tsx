0665583d61b635b410640358b9b0ac45a9c33ee8
Mike <slavik433@gmail.com>
2022-02-14 17:10:07 +0300

    lesson 10 connect Redux

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -26,7 +26,7 @@ export type TasksStateType = {
 }
 
 
-function AppWithReducer() {
+function AppWithRedux() {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
@@ -138,4 +138,4 @@ function AppWithReducer() {
     );
 }
 
-export default AppWithReducer;
+export default AppWithRedux;
